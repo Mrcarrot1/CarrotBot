20935ecbc38c02c124efbd7860ee644bb731d81f
Caleb Harper <28740091+Mrcarrot1@users.noreply.github.com>
2022-06-14 13:39:01 -0500

    Minor improvements to leveling, fixed a typo, and replaced the top.gg link

--- DSharpPlus/CarrotBot/CommandsNext/UngroupedCommands.cs
+++ DSharpPlus/CarrotBot/CommandsNext/UngroupedCommands.cs
@@ -330,7 +330,7 @@ namespace CarrotBot.Commands
             eb.AddField("Guilds", $"{Utils.GuildCount}", true);
             eb.AddField("Current Version", $"v{Utils.currentVersion}", true);
             //eb.AddField("DSharpPlus Version", $"v4.0.1");
-            eb.AddField("Invite/Vote Link", "https://top.gg/bot/389513870835974146");
+            eb.AddField("Invite/Vote Link", "https://discord.bots.gg/bots/389513870835974146");
             eb.AddField("Support Server", "https://discord.gg/wHPwHu7");
             eb.AddField("Source Repository", "https://github.com/Mrcarrot1/CarrotBot");
             eb.AddField("Website", "https://carrotbot.calebmharper.com");
---
+++ DSharpPlus/CarrotBot/Modules/ReactionRoles/ReactionRole.cs
@@ -0,0 +1,35 @@
+//Unfinished reaction role module-
+//Will be finished in a later release.
+using System;
+using System.Collections.Generic;
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+namespace CarrotBot
+{
+    public class ReactionRole
+    {
+        /// <summary>
+        /// The role to grant when the reaction is selected
+        /// </summary>
+        /// <value></value>
+        public DiscordRole Role { get; }
+        /// <summary>
+        /// The message the reaction is on
+        /// </summary>
+        /// <value></value>
+        public DiscordMessage Message { get; }
+        /// <summary>
+        /// The reaction emote to add
+        /// </summary>
+        /// <value></value>
+        public DiscordEmoji Emote { get; }
+
+        public ReactionRole(DiscordRole role, DiscordMessage message, DiscordEmoji emote)
+        {
+            Role = role;
+            Message = message;
+            Emote = emote;
+        }
+    }
+}
