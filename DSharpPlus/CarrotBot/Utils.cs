f8e932c232b2b109f6d6750dbfb110dfb6919b4f
Caleb Harper <28740091+Mrcarrot1@users.noreply.github.com>
2022-08-14 17:03:46 -0500

    Remove unused testing command

--- DSharpPlus/CarrotBot/Utils.cs
+++ DSharpPlus/CarrotBot/Utils.cs
@@ -16,7 +16,7 @@ namespace CarrotBot
 {
     public static class Utils
     {
-        private static readonly string version = "1.4.0";
+        private static readonly string version = "1.4.1";
         public static readonly string currentVersion = Program.isBeta ? $"{version}(beta)" : version;
         public static string yyMMdd = DateTime.Now.ToString("yyMMdd");
         public static DateTimeOffset startTime = DateTimeOffset.Now;
